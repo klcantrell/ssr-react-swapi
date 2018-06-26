fe83e57617dea5792829fd4c3149a81f7c6ceba8
Kalalau <cantrellkalalau@gmail.com>
2018-06-26 06:13:54 -0400

    moved loading spinner to render in another location

--- src/shared/Character.js
+++ src/shared/Character.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import LoadingSpinner from './LoadingSpinner';
 import { QUESTION_STATES } from './Question';
 import { randomInt } from './utils';
 
@@ -25,25 +24,18 @@ class Character extends Component {
   }
   
   render() {
-    const { questionState, imgData, imgLoaded, dataLoaded, handleLoad } = this.props;
+    const { questionState, imgData, imgLoaded, handleLoad } = this.props;
     let placeholderClasses = 'placeholder';
     if (imgLoaded) {
       placeholderClasses += ' fadeOut';
     }
     return (
         <div className="character">
-          <h1>May the force be with you</h1>
-          {dataLoaded ? (
-            <React.Fragment>
-              <h2 className="game-message">{this.renderMessage(questionState)}</h2>
-              <div className="hero-img">
-                <img className="full" onLoad={handleLoad} src={imgData.src} ref={node => this.fullImg = node}/>
-                <img className={placeholderClasses} src={imgData.placeholder} />
-              </div>
-            </React.Fragment>
-          ) :
-            <LoadingSpinner />
-          }
+          <h2 className="game-message">{this.renderMessage(questionState)}</h2>
+          <div className="hero-img">
+            <img className="full" onLoad={handleLoad} src={imgData.src} ref={node => this.fullImg = node}/>
+            <img className={placeholderClasses} src={imgData.placeholder} />
+          </div>
         </div>
     );
   }
