29d987a11f8b1f4c45d856572ebda5cfb1f53f1c
Kalalau <cantrellkalalau@gmail.com>
2018-08-19 19:00:01 -0400

    included branding

--- src/server/index.js
+++ src/server/index.js
@@ -46,7 +46,9 @@ const INDEX_TO_IMGDATA_MAP = {
 
 const app = express();
 
-// app.use(express.static("public"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(express.static("public"));
+}
 
 app.get("/", (req, res, next) => {
   const correctCharacter = CHARACTER_INDICES[randomInt(CHARACTER_INDICES.length - 1)];
@@ -61,17 +63,35 @@ app.get("/", (req, res, next) => {
           <!DOCTYPE html>
           <html>
             <head>
+              <title>SSR with React and SWAPI</title>
+              <style>${styles.toString()}</style>
+              <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+              <link rel="apple-touch-icon-precomposed" sizes="180x180" href="${__STATIC_URL__}apple-touch-icon.png">
+              <link rel="icon" type="image/png" sizes="32x32" href="${__STATIC_URL__}favicon-32x32.png">
+              <link rel="icon" type="image/png" sizes="16x16" href="${__STATIC_URL__}favicon-16x16.png">
+              <link rel="manifest" href="${__STATIC_URL__}site.webmanifest">
+              <link rel="mask-icon" href="${__STATIC_URL__}safari-pinned-tab.svg" color="#5bbad5">
+              <link rel="shortcut icon" href="${__STATIC_URL__}favicon.ico" type="image/ico">
+              <meta name="msapplication-TileColor" content="#2d89ef">
+              <meta name="theme-color" content="#1800e3">
               <meta charset="UTF-8">
               <meta name="viewport" content="width=device-width, initial-scale=1.0">
               <meta http-equiv="X-UA-Compatible" content="ie=edge">
-              <title>SSR with React and SWAPI</title>
               <script src="${__STATIC_URL__}bundle.js" defer></script>
               <script>window.__INITIAL_DATA__ = ${serialize(initialData)}</script>
-              <style>${styles.toString()}</style>
             </head>
 
             <body>
               <div id="app" class="column-layout">${markup}</div>
+              <div class="k-tag">
+                <svg width="50" height="50" viewBox="0 0 300 500">
+                  <path
+                    fill="rgb(255, 156, 0)" stroke="black" stroke-width="10"
+                    d="M 152.77,315.36 C 229.84,278.14 252.00,249.33 252.00,229.39 252.00,216.53 243.14,209.00 228.97,209.00 205.48,209.00 171.38,227.17 134.61,264.39 134.61,264.39 107.58,404.00 107.58,404.00 107.58,404.00 81.89,404.00 81.89,404.00 81.89,404.00 141.69,95.22 141.69,95.22 141.69,95.22 168.27,93.00 168.27,93.00 168.27,93.00 139.48,240.02 139.48,240.02 175.36,203.48 212.58,185.00 240.92,185.00 265.95,185.00 280.00,199.03 280.00,219.64 280.00,248.44 253.08,281.69 185.11,315.36 185.11,315.36 269.50,401.78 269.50,401.78 269.50,401.78 239.44,406.00 239.44,406.00 239.44,406.00 152.77,315.36 152.77,315.36 Z"
+                  >
+                </svg>
+                <h5>Star Wars Guess Who</h5>
+              </div>
             </body>
           </html>
         `,
@@ -110,8 +130,10 @@ app.get("/", (req, res, next) => {
       .catch(next);
   });
 
-// app.listen(3000, () => {
-//   console.log('App server started');
-// });
+if (process.env.NODE_ENV === 'development') {
+  app.listen(3000, () => {
+    console.log('App server started');
+  });
+}
 
 module.exports.handler = serverless(app);
