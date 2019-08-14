9389f37f226e2854c26e6a04b03a756fe6a0c4bf
klcantrell <cantrellkalalau@gmail.com>
2019-08-13 20:50:43 -0400

    deactivate db and sign up service for now

--- src/shared/Score.js
+++ src/shared/Score.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import LoadingSpinner from './LoadingSpinner';
 import { signup, signin, updateScore, getScore } from '../browser/api';
 
-const Message = ({user, score, errorMessage, awaitingResponse}) => {
+const Message = ({ user, score, errorMessage, awaitingResponse }) => {
   return (
     <React.Fragment>
       <h3 className="score__header">{`Streak: ${score}`}</h3>
       {awaitingResponse ? (
-        <LoadingSpinner style={{ width: 30, height: 30, margin: '10px auto' }} />
+        <LoadingSpinner
+          style={{ width: 30, height: 30, margin: '10px auto' }}
+        />
+      ) : user ? (
+        <p className="score__message">
+          Signed in as: <strong>{user.email}</strong>
+        </p>
       ) : (
-        user ? (
-          <p className="score__message">Signed in as: <strong>{user.email}</strong></p>
-        ) : (
-          <p className="score__message">{errorMessage || 'Sign up to save your streak'}</p>
-        )
+        <p className="score__message">
+          {/* {errorMessage || 'Sign up to save your streak'} */}
+          Score service is currently not available
+        </p>
       )}
     </React.Fragment>
   );
@@ -31,15 +36,17 @@ class Score extends Component {
     awaitingResponse: false,
   };
   componentDidMount() {
-    if (localStorage.getItem('token') && localStorage.getItem('token') !== 'undefined') {
-      getScore()
-        .then(({user}) => {
-          this.props.updateScoreOnLoginLogout(user.score);
-          this.setState({
-            token: localStorage.getItem('token'),
-            user,
-          });
+    if (
+      localStorage.getItem('token') &&
+      localStorage.getItem('token') !== 'undefined'
+    ) {
+      getScore().then(({ user }) => {
+        this.props.updateScoreOnLoginLogout(user.score);
+        this.setState({
+          token: localStorage.getItem('token'),
+          user,
         });
+      });
     }
   }
   componentDidUpdate(prevProps, prevState) {
@@ -59,7 +66,7 @@ class Score extends Component {
       formVisible: false,
     });
   }
-  handleLoggedin = ({user, token, error}) => {
+  handleLoggedin = ({ user, token, error }) => {
     if (error) {
       return this.setState({
         errorMessage: error,
@@ -67,13 +74,13 @@ class Score extends Component {
       });
     }
     localStorage.setItem('token', token);
-    this.props.updateScoreOnLoginLogout(user.score)
+    this.props.updateScoreOnLoginLogout(user.score);
     this.setState({
       user,
       token,
       awaitingResponse: false,
     });
-  }
+  };
   handleSubmit = e => {
     e.preventDefault();
     const { emailInput, passwordInput, isSigningUp } = this.state;
@@ -83,12 +90,10 @@ class Score extends Component {
       awaitingResponse: true,
     });
     if (isSigningUp) {
-      return signup(emailInput, passwordInput, score)
-        .then(this.handleLoggedin);
+      return signup(emailInput, passwordInput, score).then(this.handleLoggedin);
     }
-    return signin(emailInput, passwordInput)
-      .then(this.handleLoggedin);
-  }
+    return signin(emailInput, passwordInput).then(this.handleLoggedin);
+  };
   handleLogout = () => {
     localStorage.removeItem('token');
     this.props.updateScoreOnLoginLogout(0);
@@ -96,26 +101,26 @@ class Score extends Component {
       token: undefined,
       user: undefined,
     });
-  }
-  showForm = ({isSigningUp}) => {
+  };
+  showForm = ({ isSigningUp }) => {
     this.setState({
       formVisible: true,
       isSigningUp,
     });
-  }
+  };
   hideForm = e => {
     e.preventDefault();
     this.setState({
       formVisible: false,
     });
-  }
+  };
   updateField = e => {
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
-  }
+  };
   render() {
-    const { 
+    const {
       user,
       emailInput,
       passwordInput,
@@ -123,10 +128,11 @@ class Score extends Component {
       isSigningUp,
       token,
       errorMessage,
-      awaitingResponse } = this.state;
+      awaitingResponse,
+    } = this.state;
     return (
       <div className="score">
-        <Message 
+        <Message
           user={user}
           score={this.props.score}
           errorMessage={errorMessage}
@@ -137,7 +143,7 @@ class Score extends Component {
         ) : (
           <React.Fragment>
             <form className={`${formVisible ? 'column-layout' : 'hidden'}`}>
-              <input 
+              <input
                 className="score__form-item"
                 placeholder="email"
                 type="email"
@@ -145,7 +151,7 @@ class Score extends Component {
                 name="emailInput"
                 onChange={this.updateField}
               />
-              <input 
+              <input
                 className="score__form-item"
                 placeholder="password"
                 type="password"
@@ -156,29 +162,42 @@ class Score extends Component {
               <div className="score__form-item">
                 <button type="button" onClick={this.hideForm}>
                   <svg width="15" height="12" viewBox="0 0 492 492">
-                    <path d="M464.344,207.418l0.768,0.168H135.888l103.496-103.724c5.068-5.064,7.848-11.924,7.848-19.124
+                    <path
+                      d="M464.344,207.418l0.768,0.168H135.888l103.496-103.724c5.068-5.064,7.848-11.924,7.848-19.124
                       c0-7.2-2.78-14.012-7.848-19.088L223.28,49.538c-5.064-5.064-11.812-7.864-19.008-7.864c-7.2,0-13.952,2.78-19.016,7.844
                       L7.844,226.914C2.76,231.998-0.02,238.77,0,245.974c-0.02,7.244,2.76,14.02,7.844,19.096l177.412,177.412
                       c5.064,5.06,11.812,7.844,19.016,7.844c7.196,0,13.944-2.788,19.008-7.844l16.104-16.112c5.068-5.056,7.848-11.808,7.848-19.008
                       c0-7.196-2.78-13.592-7.848-18.652L134.72,284.406h329.992c14.828,0,27.288-12.78,27.288-27.6v-22.788
-                      C492,219.198,479.172,207.418,464.344,207.418z"/>
+                      C492,219.198,479.172,207.418,464.344,207.418z"
+                    />
                   </svg>
                 </button>
-                <button onClick={this.handleSubmit}>{isSigningUp ? 'Sign up' : 'Sign in'}</button>
+                <button onClick={this.handleSubmit}>
+                  {isSigningUp ? 'Sign up' : 'Sign in'}
+                </button>
               </div>
-            </form> 
+            </form>
             <div>
               {formVisible ? null : (
                 <React.Fragment>
-                  <button onClick={() => this.showForm({isSigningUp: true})}>Sign up</button>
-                  <button onClick={() => this.showForm({isSigningUp: false})}>Sign in</button>
+                  <button
+                    disabled
+                    onClick={() => this.showForm({ isSigningUp: true })}
+                  >
+                    Sign up
+                  </button>
+                  <button
+                    disabled
+                    onClick={() => this.showForm({ isSigningUp: false })}
+                  >
+                    Sign in
+                  </button>
                 </React.Fragment>
               )}
             </div>
           </React.Fragment>
         )}
       </div>
-      
     );
   }
 }
